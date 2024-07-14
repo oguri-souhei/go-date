8f8a16b57ef9ae93c165a0da9b6aa33ff71a97ac
souhei.oguri <souhei.oguri@gmail.com>
2024-07-14 18:12:08 +0900

    ✨ Add sql driver methods.

---
+++ sql_driver.go
@@ -0,0 +1,46 @@
+package date
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"time"
+)
+
+// Value returns a driver Value.
+// Value must not panic.
+func (d *Date) Value() (driver.Value, error) {
+	return d.String(), nil
+}
+
+// Scan assigns a value from a database driver.
+// The src value will be of one of the following types:
+//
+//	int64
+//	float64
+//	bool
+//	[]byte
+//	string
+//	time.Time
+//	nil - for NULL values
+//
+// An error should be returned if the value cannot be stored
+// without loss of information.
+//
+// Reference types such as []byte are only valid until the next call to Scan
+// and should not be retained. Their underlying memory is owned by the driver.
+// If retention is necessary, copy their values before the next call to Scan.
+func (d *Date) Scan(src any) error {
+	switch v := src.(type) {
+	case string:
+		newVal, err := Parse(v)
+		if err != nil {
+			return fmt.Errorf("failed to parse string %+v to date", v)
+		}
+		*d = newVal
+	case time.Time:
+		d.t = v
+	default:
+		return fmt.Errorf("unexpected date type %+t", v)
+	}
+	return nil
+}
---
+++ sql_driver_test.go
@@ -0,0 +1,76 @@
+package date
+
+import (
+	"database/sql/driver"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDate_Value(t *testing.T) {
+	tests := []struct {
+		name string
+		date Date
+		want driver.Value
+	}{
+		{
+			name: "returns driver.Value",
+			date: New(2024, 7, 13),
+			want: "2024-07-13",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.date.Value()
+			assert.Equal(t, tt.want, got)
+			assert.Nil(t, err)
+		})
+	}
+}
+
+func TestDate_Scan(t *testing.T) {
+	tests := []struct {
+		name    string
+		date    Date
+		args    any
+		want    Date
+		wantErr bool
+	}{
+		{
+			name: "when src is string",
+			date: Date{},
+			args: "2024-07-13",
+			want: New(2024, 7, 13),
+		},
+		{
+			name:    "when src is string and invalid format",
+			date:    Date{},
+			args:    "hoge",
+			wantErr: true,
+		},
+		{
+			name: "when src is time",
+			date: Date{},
+			args: time.Date(2024, 7, 14, 0, 0, 0, 0, time.UTC),
+			want: New(2024, 7, 14),
+		},
+		{
+			name:    "when src is int",
+			date:    Date{},
+			args:    10,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.date.Scan(tt.args)
+			if !tt.wantErr {
+				assert.Equal(t, tt.want, tt.date)
+				assert.Nil(t, err)
+			} else {
+				assert.Error(t, err)
+			}
+		})
+	}
+}
